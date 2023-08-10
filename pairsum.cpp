a2f248eb773bc87f110c14e0485ec3738aca055a
jawaabi123 <115395173+jawaabi123@users.noreply.github.com>
2023-08-10 18:57:53 +0530

    Update pairsum.cpp

--- pairsum.cpp
+++ pairsum.cpp
@@ -3,7 +3,6 @@ https://www.codingninjas.com/studio/problems/pair-sum_697295?source=youtube&camp
 code:
 
 #include <bits/stdc++.h>
-
 vector<vector<int>> pairSum(vector<int> &arr, int s)
 {
       sort(arr.begin(),arr.end());
@@ -22,5 +21,29 @@ vector<vector<int>> pairSum(vector<int> &arr, int s)
             }
          }
       }
+      return ans;                            
+}
+
+code2(map logic):
+
+vector<vector<int>> pairSum(vector<int> &arr, int s)
+{
+      
+      vector<vector<int>>ans;
+      unordered_map<int,int>mpp;
+      int n=arr.size();
+      for(int i=0;i<n;i++)
+      {
+         int count=mpp[s-arr[i]];
+         vector<int>pair;
+         pair.push_back(min(arr[i],s-arr[i]));
+         pair.push_back(max(arr[i],s-arr[i]));
+         while(count--)
+         {
+            ans.push_back(pair);
+         }
+         mpp[arr[i]]++;
+      }
+      sort(ans.begin(),ans.end());
       return ans;                            
 }
