c12f3ecb6c844eb4b7948091d209046ad9a22f80
jawaabi123 <115395173+jawaabi123@users.noreply.github.com>
2023-08-26 11:59:51 +0530

    Update next_greater_ele.cpp

--- next_greater_ele.cpp
+++ next_greater_ele.cpp
@@ -27,3 +27,44 @@ vector<int> nextGreaterElement(vector<int>& arr, int n)
 	
 	return ans;
 }
+
+https://leetcode.com/problems/next-greater-element-i/submissions/
+
+code:
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
+    {
+        int s1=nums1.size();
+        int s2=nums2.size();
+        stack<int>inter;
+        vector<int>ans(s1,-1);
+        unordered_map<int,int>mpp;
+        for(int i=s2-1;i>=0;i--)
+        {
+            while(!inter.empty() && nums2[i]>=inter.top())
+            {
+                inter.pop();
+            }
+            if(inter.empty())
+            {
+                mpp[nums2[i]]=-1;
+            }
+            else
+            {
+                mpp[nums2[i]]=inter.top();
+            }
+            inter.push(nums2[i]);
+        }
+        
+        for(int i=0;i<s1;i++)
+        {
+            if(mpp.find(nums1[i])!=mpp.end())
+            {
+                ans[i]=mpp[nums1[i]];
+            }
+        }
+        return ans;
+        
+    }
+};
