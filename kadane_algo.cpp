ce5ecdb804a3ceee2d1e671f2eae308aa2e7b2a8
jawaabi123 <115395173+jawaabi123@users.noreply.github.com>
2023-09-04 00:38:29 +0530

    Create kadane_algo.cpp

---
+++ kadane_algo.cpp
@@ -0,0 +1,68 @@
+https://leetcode.com/problems/maximum-subarray/
+
+code:
+
+brute:
+
+long long maxSubarraySum(vector<int> arr, int n)
+{
+    int maxsum=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            int sum=0;
+            for(int k=i;k<=j;k++)
+            {
+                sum+=arr[k];
+                if (sum > maxsum) {
+                  maxsum = sum;
+                }
+            }
+        }
+    }
+    return maxsum;
+}
+
+better:
+
+long long maxSubarraySum(vector<int> arr, int n)
+{
+    int maxsum=0;
+    for(int i=0;i<n;i++)
+    {
+        int sum=0;
+        for(int j=i;j<n;j++)
+        {
+            sum+=arr[j];
+            if (sum > maxsum) 
+            {
+               maxsum = sum;
+            }
+        }
+    }
+    return maxsum;
+}
+
+optimal:
+
+long long maxSubarraySum(vector<int> arr, int n)
+{
+    long long max=INT_MIN;
+    long long sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+        if(sum<0)
+        {
+            sum=0;
+        }
+        if(sum>max)
+        {
+            max=sum;
+        }
+        
+    }
+    return max;
+}
+
