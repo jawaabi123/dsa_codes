c0f5e0082e0442b1223d53c48d1110a20dcec68d
jawaabi123 <115395173+jawaabi123@users.noreply.github.com>
2024-09-17 23:12:37 +0530

    Update search2dmatrix.cpp

--- search2dmatrix.cpp
+++ search2dmatrix.cpp
@@ -1,6 +1,25 @@
 https://leetcode.com/problems/search-a-2d-matrix/description/
 
+1)brute:
+class Solution{
+public:
+    bool searchMatrix(vector<vector<int>> &mat, int target)
+    {
+        for(int i=0;i<mat.size();i++)
+        {
+            for(int j=0;j<mat[0].size();j++)
+            {
+                if(mat[i][j]==target) return true;
+            }
+        }
+        return false;
+    }
+};
+2)better:
 
+
+
+3)optimal:
 code:
 
 class Solution {
