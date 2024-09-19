36dceb37d8818efa5618c41d071ba468ebff89c5
jawaabi123 <115395173+jawaabi123@users.noreply.github.com>
2024-09-19 21:33:21 +0530

    Update 3sum.cpp

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,9 +1,57 @@
 https://www.codingninjas.com/studio/problems/three-sum_6922132?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=1
+https://leetcode.com/problems/3sum/description/
+1)brute approach:
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) 
+    {
+        set<vector<int>> storage;
+        vector<vector<int>> ans;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                for(int k=j+1;k<n;k++)
+                {
+                    if(nums[i]+nums[j]+nums[k]==0) storage.insert({nums[i],nums[j],nums[k]});
+                }
+            }
+        }
+        for(auto it:storage)
+        {
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
 
+2)better approach:
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) 
+    {
+        set<vector<int>>storage;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            set<int>temp;
+            for(int j=i+1;j<n;j++)
+            {
+               int third=-(nums[i]+nums[j]);
+               if(temp.find(third)!=temp.end())
+               {
+                  storage.insert({nums[i],nums[j],third});
+               }
+               temp.insert(nums[j]);
+            }
+        }
+        vector<vector<int>>ans(storage.begin(),storage.end());
+        return ans;
+    }
+};
 
-
-code:
-
+3)optimal approach:
 #include<bits/stdc++.h>
 vector<vector<int>> triplet(int n, vector<int> &arr)
 {
